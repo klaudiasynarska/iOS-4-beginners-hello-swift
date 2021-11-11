f1ce8425f28eb3fdeb73e9d26b73d04360610d71
Klaudia Synarska <klaudiasynarska@wp.pl>
2021-11-11 02:17:34 +0100

    Added Factorization without tests

--- HelloSwift/Factorization.swift
+++ HelloSwift/Factorization.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+enum Result {
+    case prime
+    case composite(factors: Array<Int>)
+}
+
+class Factorization {
+    func factorize(_ number: Int) -> Result? {
+        if number == 1 {
+            return nil
+        }
+
+        var factors: [Int] = []
+
+        for i in 1...number {
+            if number % i == 0 {
+                factors.append(i)
+            }
+        }
+
+        factors.removeFirst()
+        factors.removeLast()
+
+        if factors.isEmpty {
+            return Result.prime
+        } else {
+            return Result.composite(factors: factors)
+        }
+    }
+}
