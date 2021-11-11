585132c8756f20a1865b9829c7832a7172dd95df
Klaudia Synarska <klaudiasynarska@wp.pl>
2021-11-11 01:19:00 +0100

    Added StringTools

--- HelloSwift/StringTools.swift
+++ HelloSwift/StringTools.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+class StringTools {
+    func countOccurrences(character: String, from: String, caseSensitive: Bool = true) -> Int {
+        if caseSensitive {
+            var count = 0
+            
+            for letter in from {
+                let letterString = String(letter)
+               
+                if letterString == character {
+                    count = count + 1
+                }
+            }
+
+            return count
+        } else {
+            var count = 0
+            let lowercasedFrom = from.lowercased()
+            let lowercasedCharacter = character.lowercased()
+
+            for letter in lowercasedFrom {
+                let letterString = String(letter)
+               
+                if letterString == lowercasedCharacter {
+                    count = count + 1
+                }
+            }
+
+            return count
+        }
+    }
+}
